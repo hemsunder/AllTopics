247403b10dc2ddf48eea44ace5c341553e6ce50a
Emma Paris <hemsundergoud@gmail.com>
2019-07-15 23:02:01 +0530

    IE Launch Added

--- AllTopics/src/AllTopics/LaunchingInternetExplorerBrowser.java
+++ AllTopics/src/AllTopics/LaunchingInternetExplorerBrowser.java
@@ -2,20 +2,18 @@ package AllTopics;
 
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.Test;
 
-public class LaunchinChromeBrowser {
-	
-	WebDriver driver;
+public class LaunchingInternetExplorerBrowser {
+
+WebDriver driver;
 	
 	@Test
-	public void LaunchChrome(){
+	public void LaunchIE(){
 		
-		System.setProperty("webdriver.chrome.driver", "D:/Automation Softwares/Selenium Drivers/"
-				+ "chromedriver_win32 latest version 2.36/chromedriver.exe");
-		
-		driver=new ChromeDriver();
+		System.setProperty("webdriver.ie.driver", "D:/Automation Softwares/Selenium Drivers/IEDriverServer.exe");
+		driver=new InternetExplorerDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -26,5 +24,4 @@ public class LaunchinChromeBrowser {
 		
 		driver.close();
 	}
-
 }
