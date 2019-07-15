45c451f6dccdfdc04ff48f50e64f4b37de74c4b0
Emma Paris <hemsundergoud@gmail.com>
2019-07-15 22:52:42 +0530

    Launching ChromeBrowser Added

---
+++ AllTopics/src/AllTopics/LaunchinChromeBrowser.java
@@ -0,0 +1,31 @@
+package AllTopics;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LaunchinChromeBrowser {
+	
+	WebDriver driver;
+	
+	@Test
+	public void LaunchChrome(){
+		
+		System.setProperty("webdriver.chrome.driver", "D:/Automation Softwares/Selenium Drivers/"
+				+ "chromedriver_win32 latest version 2.36/chromedriver.exe");
+		
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().deleteAllCookies();
+		driver.get("https://www.google.com/");
+		String Title=driver.getTitle();
+		System.out.println("Title of the page is "+Title);
+		
+		driver.close();
+	}
+
+}
