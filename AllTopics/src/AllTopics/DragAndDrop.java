d587c7b5f992e6e31c9b0962b6b4ac26b3630c4c
Emma Paris <hemsundergoud@gmail.com>
2019-07-16 22:54:32 +0530

    DragAndDrop added

---
+++ AllTopics/src/AllTopics/DragAndDrop.java
@@ -0,0 +1,40 @@
+package AllTopics;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class DragAndDrop {
+	WebDriver driver;
+	
+	@Test
+	public void draganddrop(){
+		
+		System.setProperty("webdriver.chrome.driver", "D:/Automation Softwares/Selenium Drivers/"
+				+ "chromedriver_win32 latest version 2.36/chromedriver.exe");
+		
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("https://jqueryui.com/droppable/");
+		
+		driver.switchTo().frame(0);
+		WebElement source=driver.findElement(By.xpath("//div[@id='draggable']"));
+		WebElement target=driver.findElement(By.xpath("//div[@id='droppable']"));
+		
+		Actions act=new Actions(driver);
+		act.clickAndHold(source).moveToElement(target).release().build().perform();
+		
+		
+		
+		
+	}
+
+}
