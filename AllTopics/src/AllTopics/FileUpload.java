ba88a3af696287f0cb9a771881d527ae1cc9cc3c
Emma Paris <hemsundergoud@gmail.com>
2019-07-16 08:00:29 +0530

    FileUpload added

---
+++ AllTopics/src/AllTopics/FileUpload.java
@@ -0,0 +1,30 @@
+package AllTopics;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class FileUpload {
+	
+WebDriver driver;
+	
+	@Test
+	public void LaunchChrome(){
+		
+		System.setProperty("webdriver.chrome.driver", "D:/Automation Softwares/Selenium Drivers/"
+				+ "chromedriver_win32 latest version 2.36/chromedriver.exe");
+		
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().deleteAllCookies();
+		driver.get("https://html.com/input-type-file/");
+		driver.findElement(By.xpath("//input[@value='fileupload']")).sendKeys("C:/Users/Admin/Downloads/June-2019.pdf");
+		
+	}
+
+
+}
