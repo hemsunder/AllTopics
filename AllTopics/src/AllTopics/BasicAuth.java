74facfabdd6de1fbb6db4955e78e52b465834907
hemsunder <hemsundergoud@gmail.com>
2019-11-14 07:50:43 +0530

    BasicAuth added

---
+++ AllTopics/src/AllTopics/BasicAuth.java
@@ -0,0 +1,32 @@
+package AllTopics;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicAuth { 
+	
+	public static void main(String[] args) throws Exception {
+		
+		WebDriver driver;
+		System.setProperty("webdriver.chrome.driver", "C:\\Softwares&Drivers\\WebDriver\\chromedriver_win32\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		
+		//Dynamic Wait
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		
+		driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth");
+		Thread.sleep(5000);
+		String paramessage=driver.findElement(By.cssSelector("p")).getText();
+		System.out.println("The Message is "+paramessage);
+		
+		driver.close();
+		
+	}
+
+}
